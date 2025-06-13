47558c9a38b1ebadf0f13ad64c43ecc8e04092c1
nitaalvarez1993 <nitaalvarez1993@gmail.com>
2025-06-13 06:35:37 -0400

    The main app and main controller are updated to modify the main view of the app

--- src/main/java/com/smartrough/app/MainApp.java
+++ src/main/java/com/smartrough/app/MainApp.java
@@ -1,20 +1,33 @@
 package com.smartrough.app;
 
+import com.smartrough.app.dao.Database;
+import com.smartrough.app.util.ViewNavigator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
+
 	@Override
 	public void start(Stage stage) throws Exception {
 		// Crea la base de datos y tablas si no existen
-		com.smartrough.app.dao.Database.initializeSchema();
+		Database.initializeSchema();
 
+		// Carga MainView como layout base
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/smartrough/app/MainView.fxml"));
-		Scene scene = new Scene(loader.load(), 800, 600);
+		BorderPane rootLayout = loader.load();
+
+		// Establece la referencia del contenedor principal en ViewNavigator
+		ViewNavigator.setMainLayout(rootLayout);
+
+		// Carga por defecto el listado de compañías
+		ViewNavigator.loadView("CompanyListView.fxml");
+
+		// Configura y muestra
+		stage.setScene(new Scene(rootLayout, 800, 600));
 		stage.setTitle("Client Manager App");
-		stage.setScene(scene);
 		stage.show();
 	}
 
--- src/main/java/com/smartrough/app/MainController.java
+++ src/main/java/com/smartrough/app/MainController.java
@@ -15,9 +15,25 @@ public class MainController {
 	private Button btnClientes;
 
 	@FXML
+	public void initialize() {
+		// Al iniciar el controlador, carga el listado de compañías
+		loadCompanyList();
+	}
+
+	@FXML
 	private void showCompanyForm() {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/smartrough/app/form/CompanyForm.fxml"));
+			Parent view = loader.load();
+			root.setCenter(view);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void loadCompanyList() {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/smartrough/app/view/CompanyListView.fxml"));
 			Parent view = loader.load();
 			root.setCenter(view);
 		} catch (Exception e) {
